eb6ac75b698c71e7aa1e8c1df3a2ca1ccc31c68a
sleklere <santiagoleklere@gmail.com>
2024-04-11 12:01:56 -0300

    text_regex.py reformatted and README.md

--- text_regex.py
+++ text_regex.py
@@ -1,7 +1,6 @@
 import re
 import json
 import datetime
-
 class bcolors:
     HEADER = "\033[95m"
     OKBLUE = "\033[94m"
@@ -13,35 +12,33 @@ class bcolors:
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
 
-json_file = open("test_data_1.json", "r")
-result = json.load(json_file)
+class TextFormatter:
+
+  @staticmethod
+  def color_string(color, string):
+      return color + string + bcolors.ENDC
+
+class DataAnalyzer:
+  def __init__(self, filepath):
+    with open(filepath, "r") as file:
+       self.data = json.load(file)
+
+  @staticmethod
+  def split_sentences(text):
+      return re.split(r'(?<=[.!?])\s+', text)
+
+  def search(self, substring):
+    print("\n")
+    for segment in self.data["segments"]:
+      if substring in segment["text"]:
+        colored_str = TextFormatter.color_string(bcolors.OKGREEN, substring)
+        segment_text = segment["text"].replace(substring, colored_str)
+        timestamp = datetime.timedelta(seconds=segment["start"])
+
+        print(f'String {colored_str} at [{timestamp}]: ', segment_text)
+        print("")
+    print("\n")
 
 
-def split_sentences(text):
-  return re.split(r'(?<=[.!?])\s+', text)  
-
-def color_string(color, string):
-  return color + string + bcolors.ENDC
-
-def color_str_in_text(str, text, color):
-  return text.replace(str, color_string(color, str))
-
-def format_time(seconds):
-  return datetime.timedelta(seconds=seconds)
-
-def search(str, segments_list):
-  print("\n")
-  for segment in segments_list:
-    segment_text = segment["text"]
-
-    if str in segment_text:
-      # format output
-      colored_str = color_string(bcolors.OKGREEN, str)
-      segment_text = segment_text.replace(str, colored_str)
-      timestamp = format_time(segment["start"])
-
-      print(f'String {colored_str} at [{timestamp}]: ', segment_text)
-      print("")
-  print("\n")
-
-search("desarrollador", result["segments"])
+analyzer = DataAnalyzer("my_data.json")
+analyzer.search("desarrollador")
